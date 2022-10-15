7143ca1a2387da44e2977bc11bd0af1778656e91
Kharukulavat <nipunk0258@gmail.com>
2022-10-15 23:52:34 +0700

    Create Exercise4_Nipun_K.py

---
+++ Exercise4_Nipun_K.py
@@ -0,0 +1,30 @@
+"""
+หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
+
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+
+--- Your Score ---
+
+Foundation English : คะแนนที่ได้
+
+General Business : คะแนนที่ได้
+
+Introduction to Computer Systems : คะแนนที่ได้
+
+Computer Programming : คะแนนที่ได้
+
+*การตั้งชื่อตัวแปรควรสื่อความหมาย ผู้อ่าน หรือ นักพัฒนาโปรแกรมท่านอื่นสามารถอ่านเข้าใจได้
+
+"""
+FE = input("Foundation English : ")
+GB = input("General Business : ")
+ITC = input("Introduction to Computer Systems: ")
+CP = input("Computer Programming : ")
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems:",ITC)
+print("Computer Programming :",CP)
